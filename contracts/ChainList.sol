1f3b33a252d248dbd9b789b9fa71ec2de7047f52
leopangchan <leopangchan@gmail.com>
2018-08-18 23:29:08 -0700

    Add buying article function

--- contracts/ChainList.sol
+++ contracts/ChainList.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.18;
 contract ChainList {
     // state variables
     address seller;
+    address buyer;
     string name;
     string description;
     uint256 price; // in wei
@@ -10,6 +11,13 @@ contract ChainList {
     // events
     event LogSellArticle(
         address indexed _seller,
+        string _name,
+        uint256 _price
+    );
+
+    event LogBuyArticle(
+        address indexed _seller,
+        address indexed _buyer,
         string _name,
         uint256 _price
     );
@@ -28,13 +36,30 @@ contract ChainList {
         LogSellArticle(seller, name, price);
     }
 
+    function buyArticle() payable public {
+        require(seller != 0x0);
+
+        require(buyer == 0x0);
+
+        require(msg.sender != seller);
+
+        require(msg.value == price);
+
+        buyer = msg.sender;
+
+        seller.transfer(msg.value);
+
+        LogBuyArticle(seller, buyer, name, price);
+    }
+
   // get an article
     function getArticle() public view returns (
         address _seller,
+        address _buyer,
         string _name,
         string _description,
         uint256 _price
     ) {
-        return (seller, name, description, price);
+        return (seller, buyer, name, description, price);
     }
 }
